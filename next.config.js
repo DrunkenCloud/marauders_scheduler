05986fe572d7617678e9cf00abe6027d3f861b7f
DrunkenCloud <drunkencloud99@gmail.com>
2025-10-20 18:29:41 +0530

    random BS go

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Exclude prisma seed file from build
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        '../prisma/seed': 'commonjs ../prisma/seed',
+      })
+    }
+    return config
+  },
+  // Exclude seed file from TypeScript compilation
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  }
+}
 
 module.exports = nextConfig
